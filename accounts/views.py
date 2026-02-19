efe354893914e783377e04c81b7a7181d29c0ec0
netripper <naseri.mani2005@gmail.com>
2026-02-20 00:11:03 +0330

    3rd commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import UserSerializer
+from django.contrib.auth import authenticate
 from .models import User
 
 class UserRegisterApiView(APIView):
@@ -16,3 +18,14 @@ class UserRegisterApiView(APIView):
         return Response(serializeData.errors)
 
 
+class UserLoginApiView(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            return Response({"message": "user logged in successfully"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "user does not exist"}, status=status.HTTP_401_UNAUTHORIZED)
+        
